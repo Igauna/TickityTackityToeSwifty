18d7fc0c1c45664434656011abe67e2eeccf923f
Iguana <igaona10001@gmail.com>
2017-07-11 13:37:33 -0500

    Added Label codenames (aka KND)

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pTurn: UILabel!
+    
+    @IBOutlet weak var winnerWinnerChickenDInner: UILabel!
+    
+    @IBOutlet weak var oneScore: UILabel!
+    
+    @IBOutlet weak var twoScore: UILabel!
+    
+    @IBOutlet weak var topLeft: UILabel!
+    
+    @IBOutlet weak var topMiddle: UILabel!
+    
+    @IBOutlet weak var topRight: UILabel!
+    
+    @IBOutlet weak var middleLeft: UILabel!
+    
+    @IBOutlet weak var middleMiddle: UILabel!
+   
+    @IBOutlet weak var middleRight: UILabel!
+    
+    @IBOutlet weak var botLeft: UILabel!
+
+    @IBOutlet weak var botMiddle: UILabel!
+    
+    @IBOutlet weak var botRight: UILabel!
+    
+ @IBOutlet var tappyTap: UITapGestureRecognizer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
